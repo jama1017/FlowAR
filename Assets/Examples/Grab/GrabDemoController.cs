3eab2b3e09ae06b4974c0b25ae6838ee062b05b7
Jiaju Ma <jiaju_ma@brown.edu>
2020-10-25 18:31:09 -0400

    disabled particle. fixing poop orientation

--- Assets/Examples/Grab/GrabDemoController.cs
+++ Assets/Examples/Grab/GrabDemoController.cs
@@ -6,14 +6,21 @@ using Portalble;
 namespace Portalble.Examples.BasicGrab {
     public class GrabDemoController : PortalbleGeneralController {
         public Transform placePrefab;
-        public float offset = 0.01f;
+        public float offset = 0.00002f;
 
         public override void OnARPlaneHit(PortalbleHitResult hit) {
             base.OnARPlaneHit(hit);
+            Transform poop = null;
 
             if (placePrefab != null) {
-                Instantiate(placePrefab, hit.Pose.position + hit.Pose.rotation * Vector3.up * offset, hit.Pose.rotation);
+                poop = Instantiate(placePrefab, hit.Pose.position + hit.Pose.rotation * Vector3.up * offset, hit.Pose.rotation);
             }
+
+            Vector3 targetPostition = new Vector3(-Camera.main.transform.position.x,
+                                       poop.position.y,
+                                       -Camera.main.transform.position.z);
+
+            poop.transform.LookAt(targetPostition);
         }
     }
 }
